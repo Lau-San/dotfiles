6a0178284efe8cf63f7fc22c9483a33fa57657e0
Lau-San <lausanch0512@gmail.com>
2024-08-02 17:04:00 -0500

    Update .config/nvim/lua/plugins/quick_add.lua

--- dot_config/nvim/lua/plugins/quick_add.lua
+++ dot_config/nvim/lua/plugins/quick_add.lua
@@ -10,6 +10,7 @@ return {
     { 'elkowar/yuck.vim' },
     { 'eraserhd/parinfer-rust', build = 'cargo build --release' },
     { 'editorconfig/editorconfig-vim' },
+    { 'wakatime/vim-wakatime', lazy = false },
     { 'jasonccox/vim-wayland-clipboard' },
     {
         'folke/todo-comments.nvim',
