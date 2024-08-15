0e0310daf5d004f68e1a856632fae3b67899b852
Lau-San <lausanch0512@gmail.com>
2024-08-14 22:44:22 -0500

    Update .config/nvim/lua/plugins/quick_add.lua

--- dot_config/nvim/lua/plugins/quick_add.lua
+++ dot_config/nvim/lua/plugins/quick_add.lua
@@ -37,6 +37,12 @@ return {
         'nvim-neorg/neorg',
         lazy = false,
         version = "*",
-        config = true
+        config = true,
+        opts = {
+            load = {
+                ['core.defaults'] = {},
+                ['core.concealer'] = {}
+            }
+        }
     }
 }
