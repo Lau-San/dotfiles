0c66fe513977e38d9c9c72aa08906eda6d84a499
Lau-San <lausanch0512@gmail.com>
2024-08-20 13:21:05 -0500

    Update .config/walker/plugins/obsidian.py

--- dot_config/walker/plugins/obsidian.py
+++ dot_config/walker/plugins/obsidian.py
@@ -1,27 +1,30 @@
 import sys
 import json
 
-text = ' '.join(sys.argv[1:])
+user_input = ' '.join(sys.argv[1:])
 
 options = []
 
-if text:
+if user_input:
     options.append({
-        'label': f'  Create note: {text}',
-        'searchable': text.lower(),
-        'exec': f'kitty on "{text}"'
+        'label': f'  Create note: {user_input}',
+        'searchable': user_input.lower(),
+        'exec': f'kitty on "{user_input}"'
     })
 
 actions = {
     '󱉼  Open vault': 'kitty oo',
-    '  Review notes': 'kitty oor'
+    '  Review notes': 'kitty oor',
+    '  Open Yesterday\'s note': "kitty nvim +':ObsidianYesterday'",
+    '  Open Today\'s note': "kitty nvim +':ObsidianToday'",
+    '  Open Tomorrow\'s note': "kitty nvim +':ObsidianTomorrow'",
 }
 
 for k, v in actions.items():
     options.append({
         'label': k,
-        'searchable': text.lower(),
-        'exec': v
+        'searchable': k.lower(),
+        'exec': v,
     })
 
 print(json.dumps(options))
