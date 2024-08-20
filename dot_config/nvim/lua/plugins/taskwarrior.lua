1b955a54913aa0842f0d1f4defba6f6e432774b5
Lau-San <lausanch0512@gmail.com>
2024-08-20 16:51:32 -0500

    Update .config/nvim/lua/plugins/taskwarrior.lua

--- dot_config/nvim/lua/plugins/taskwarrior.lua
+++ dot_config/nvim/lua/plugins/taskwarrior.lua
@@ -3,7 +3,7 @@ return {
         'ribelo/taskwarrior.nvim',
         lazy = false,
         keys = {
-            { '<leader>t', function() require'taskwarrior.nvim'.browser() end }
+            { '<leader>t', function() require'taskwarrior'.browser() end }
         }
     }
 }
