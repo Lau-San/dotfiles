8ab91ba7341bd13afaeda14575806b4212a02225
Lau-San <lausanch0512@gmail.com>
2024-08-19 23:31:31 -0500

    Update .config/walker/plugins/tasks.py Update .config/walker/themes/tokyonight-centered.css

--- dot_config/walker/plugins/tasks.py
+++ dot_config/walker/plugins/tasks.py
@@ -1,38 +1,66 @@
 import sys
+import subprocess
 import json
 
-# Text being enterd by user on prompt
+# Get user input in prompt
 input_text = ' '.join(sys.argv[1:])
 
-# Window Class for kitty windows
-w_class = 'ls-tasks'
+# VIT command to open reports
+vit_cmd = 'kitty --class="ls-tasks" vit'
 
 # List that will contain the entries for Walker
 entries = []
 
-# If the input isn't empty, add an "Add task" option
-if input_text:
-    entries.append({
-        'label': f'Add task: {input_text}',
-        'searchable': input_text.lower(),
-        'exec': f'task add "{input_text}" +new'
-    })
-
-# Add all options to view tasks
+# General reports options
 actions = {
-    'Ready': f'kitty --class="{w_class}" vit ready',
-    'Inbox': f'kitty --class="{w_class}" vit inbox',
-    'Next': f'kitty --class="{w_class}" vit next',
-    'Active': f'kitty --class="{w_class}" vit active',
-    'Completed': f'kitty --class="{w_class}" vit completed',
-    'Open project ': 'task _projects | walker - -dmenu - n - -theme = tokyonight-centered'
+    'Ready': f'{vit_cmd} ready',
+    'Inbox': f'{vit_cmd} inbox',
+    'Next': f'{vit_cmd} next',
+    'Active': f'{vit_cmd} active',
+    'Completed': f'{vit_cmd} completed'
 }
-for k, v in actions.items():
-    entries.append({
-        'label': k,
-        'searchable': k.lower(),
-        'exec': v
-    })
 
-# print(projects)
+
+def get_projects() -> dict:
+    """Get Taskwarrior projects and return a dictionary where the keys are
+    pretty names (title case) and the values are their name in Taskwarrior."""
+    # Get all Taskwarrior projects
+    tw_projects = subprocess.run(['task', '_projects'],
+                                 stdout=subprocess.PIPE,
+                                 text=True).stdout.strip().split()
+    # Format project names
+    projects_pretty = list(map(lambda s: ' '.join(s.split('-')).title(),
+                               tw_projects))
+    # Map pretty project names to their Taskwarrior name
+    projects = dict(zip(projects_pretty, tw_projects))
+
+    return projects
+
+
+def generate_entries(i, a, p):
+    # If the input isn't empty, add an "Add task" option
+    if input_text:
+        entries.append({
+            'label': f'Add task: {input_text}',
+            'searchable': input_text.lower(),
+            'exec': f'task add "{input_text}" +new'
+        })
+
+    # Generate general report entries
+    for k, v in actions.items():
+        entries.append({
+            'label': k,
+            'searchable': k.lower(),
+            'exec': v
+        })
+
+    # Generate project report entries
+    for k, v in get_projects().items():
+        entries.append({
+            'label': f'Project: {k}',
+            'searchable': f'project: {k.lower()}',
+            'exec': f'{vit_cmd} project:{v}'
+        })
+
+
 print(json.dumps(entries))
