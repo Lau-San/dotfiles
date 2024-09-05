2cd114ef299927ac8eca33578db1efb625241d98
Lau-San <lausanch0512@gmail.com>
2024-09-05 14:38:04 -0500

    Update .config/walker/plugins/projects.py

--- dot_config/walker/plugins/projects.py
+++ dot_config/walker/plugins/projects.py
@@ -37,7 +37,7 @@ def generate_entries(simple: dict, zellij_layouts: dict) -> list:
         entries.append({
             'label': f'  {k}',
             'searchable': k.lower(),
-            'exec': f'{zj_new(v, True)}'
+            'exec': f'zellij d {v}; {zj_new(v, True)}'
         })
 
     return entries
