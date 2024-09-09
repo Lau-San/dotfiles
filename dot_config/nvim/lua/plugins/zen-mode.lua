5f61162efbe078b26aac22ff461aae10ff08f182
Lau-San <lausanch0512@gmail.com>
2024-09-09 17:22:54 -0500

    Update .config/nvim/lua/plugins/zen-mode.lua

--- dot_config/nvim/lua/plugins/zen-mode.lua
+++ dot_config/nvim/lua/plugins/zen-mode.lua
@@ -1,5 +1,17 @@
 return {
     'folke/zen-mode.nvim',
+    opts = {
+        window = {
+            options = {
+                relativenumber = false,
+                number = false,
+                list = false
+            }
+        },
+        plugins = {
+            laststatus = 0
+        }
+    },
     keys = {
         { '<leader>tz', ':ZenMode<cr>', desc = 'Toggle Zen Mode' }
     }
