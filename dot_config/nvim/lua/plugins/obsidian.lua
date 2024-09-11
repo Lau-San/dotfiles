ba792db9cc0170010277b4ca381e3afc331722f6
Lau-San <lausanch0512@gmail.com>
2024-09-10 20:33:58 -0500

    Update .config/nvim/lua/plugins/obsidian.lua

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -4,6 +4,13 @@ local function new_note()
     vim.cmd('normal! 1G')
     vim.cmd('normal! dd')
 end
+
+local function new_task()
+    local task = vim.fn.input { prompt = 'New task: ' }
+    local date = os.date('%Y-%m-%d')
+    vim.cmd('!echo "- [ ] ' .. task .. '" >> ~/Documents/personal/journal/' .. date .. '.md')
+end
+
 return {
     {
         'epwalsh/obsidian.nvim',
@@ -34,7 +41,7 @@ return {
                     -- [" "] = { char = " ", hl_group = "ObsidianTodo" },
                     -- ["x"] = { char = " ", hl_group = "ObsidianDone" },
                     -- ["/"] = { char = " ", hl_group = "ObsidianTilde" },
-                    ["-"] = { char = " ", hl_group = "ObsidianTilde" },
+                    -- ["-"] = { char = " ", hl_group = "ObsidianTilde" },
                     -- [">"] = { char = " ", hl_group = "ObsidianRightArrow" },
                     -- ["*"] = { char = "", hl_group = "ObsidianRightArrow" },
                     -- ["!"] = { char = "", hl_group = "ObsidianImportant" },
@@ -62,18 +69,20 @@ return {
             { '<leader>os', ':ObsidianQuickSwitch<cr>', desc = 'open a note' },
             { '<leader>on', new_note, desc = 'create new note' },
             { '<leader>oT', ':ObsidianTemplate note<cr>', desc = 'apply default template' },
-            { '<leader>oot', ':ObsidianToday', desc = "open today's note" },
-            { '<leader>ooT', ':ObsidianTomorrow', desc = "open tomorrow's note" },
+            { '<leader>oot', ':ObsidianToday<cr>', desc = "open today's note" },
+            { '<leader>ooT', ':ObsidianTomorrow<cr>', desc = "open tomorrow's note" },
             { '<leader>or', ':ObsidianRename<cr>', desc = 'rename note' },
             { '<leader>of', ':s/-/ /g<cr>', desc = 'format title' },
             { '<leader>ok', ':!mv "%:p" ~/Documents/personal/zettelkasten<cr>:bd<cr>', desc = 'add note to zettelkasten' },
             { '<leader>odd', ":!rm -rf '%:p'<cr>:bd<cr>", desc = 'delete current note' },
 
+            { '<leader>otn', new_task, desc = 'add task to inbox' },
             { '<leader>ott', ':s/- \\[.\\]/- \\[ \\]/<cr>', desc = 'set status to do' },
-            { '<leader>otp', ':s/- \\[.\\]/- \\[\\/\\]/<cr>', desc = 'set status in progress' },
+            { '<leader>oti', ':s/- \\[.\\]/- \\[\\/\\]/<cr>', desc = 'set status incomplete' },
             { '<leader>otd', ':s/- \\[.\\]/- \\[x\\]/<cr>', desc = 'set status done' },
             { '<leader>otc', ':s/- \\[.\\]/- \\[-\\]/<cr>', desc = 'set status cancelled' },
             { '<leader>otm', ':s/- \\[.\\]/- \\[>\\]/<cr>', desc = 'set status moved' },
+            { '<leader>otP', ':s/- \\[.\\]/- \\[*\\]/<cr>', desc = 'set pinned' },
             { '<leader>otM', ':s/- \\[.\\]/- \\[M\\]/<cr>', desc = 'set priority MUST' },
             { '<leader>otS', ':s/- \\[.\\]/- \\[S\\]/<cr>', desc = 'set priority SHOULD' },
             { '<leader>otC', ':s/- \\[.\\]/- \\[C\\]/<cr>', desc = 'set priority COULD' },
