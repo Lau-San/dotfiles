3e012e3e4aaf79f446a0a859055c3d0979550cb7
Lau-San <lausanch0512@gmail.com>
2024-09-24 14:35:51 -0500

    Update .config/nvim/lua/plugins/transparent.lua

--- dot_config/nvim/lua/plugins/transparent.lua
+++ dot_config/nvim/lua/plugins/transparent.lua
@@ -2,7 +2,7 @@ return {
     {
         'folke/tokyonight.nvim',
         opts = {
-            transparent = false,
+            transparent = true,
             styles = {
                 sidebars = 'dark',
                 floats = 'normal'
