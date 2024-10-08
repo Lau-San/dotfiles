e884bcefd3f8887c7ab39bd82f9c30036ca8ced2
Lau-San <lausanch0512@gmail.com>
2024-10-07 19:21:49 -0500

    Update .config/nvim/lua/plugins/mini.lua

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -2,7 +2,7 @@ local icons = {
     'echasnovski/mini.icons',
     opts = {
         filetype = {
-            ['lua.chezmoitmpl'] = { glyph = '' }
+            ['lua.chezmoitmpl'] = { glyph = '', hl = 'MiniIconsGreen' }
         }
     }
 }
