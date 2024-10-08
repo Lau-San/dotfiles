cdd59c1f3c52432f49dc9b3215ec65db014743f9
Lau-San <lausanch0512@gmail.com>
2024-10-08 00:22:18 -0500

    Update .config/nvim/lua/plugins/neo-tree.lua

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -2,5 +2,13 @@ return {
     'nvim-neo-tree/neo-tree.nvim',
     keys = {
         { '<leader>e', false },
+    },
+    opts = {
+        {
+            event = 'file_open_requested',
+            handler = function()
+                require('neo-tree.command').execute({ action = 'close' })
+            end
+        }
     }
 }
