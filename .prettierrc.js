93c04f8e3083c87948f8ac540925b357b58fe3ce
Lau-San <lausanch0512@gmail.com>
2025-02-28 22:22:45 -0500

    Undate 2025-02-28 22:22

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    semi: false,
+    tabWidth: 4,
+    singleAttributePerLine: true,
+}
