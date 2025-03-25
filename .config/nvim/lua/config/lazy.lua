0f1febe18fae37544d1361ee2d6169a605e67120
Lau-San <lausanch0512@gmail.com>
2025-03-25 00:47:56 -0500

    neovim: Removed lazy file code that was moved to its own file

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -17,9 +17,3 @@ require('lazy').setup({
         colorscheme = { 'tokyonight' }
     }
 })
-
--- LazyFile
-local lazy_file_events = { 'BufReadPost', 'BufNewFile', 'BufWritePre' }
-local Event = require('lazy.core.handler.event')
-Event.mappings.LazyFile = { id = 'LazyFile', event = lazy_file_events }
-Event.mappings['User LazyFile'] = Event.mappings.LazyFile
